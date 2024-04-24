e82eadaff454dfad3ddb5ff431a0431de635579f
Mattiallah Khan Safi <84840213+mattikhan@users.noreply.github.com>
2024-04-24 13:21:36 +0500

    Add files via upload

---
+++ demoMain/src/demomain/Shape.java
@@ -0,0 +1,15 @@
+package demomain;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SP21-BSE-030
+ */
+public interface Shape {
+   void draw();
+}
